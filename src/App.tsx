0f89886341cca0edd87aa3be53cddad79d1336e1
Alozi <alozimozi@gmail.com>
2025-10-09 11:28:56 +0300

    Chage stylization

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
+import styled from "styled-components";
 import WeatherCard from "./components/WeatherCard";
 import { GlobalStyle } from "./styles/GlobalStyles";
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  gap: 3rem;
+  /* background: linear-gradient(135deg, #a2d2ff 0%, #ffe5ec 100%); */
+  background: linear-gradient(to bottom right, #a2d2ff, #bde0fe, #fff1e6);
+  padding: 2rem;
+  color: #333;
+`;
+
+const Title = styled.h1`
+  font-size: 2.5rem;
+  color: #fff;
+  text-shadow: 1px 1px 4px rgba(0, 0, 0, 0.2);
+`;
 
 function App() {
   return (
     <>
-      <h1>Weather App</h1>
-      <WeatherCard />
+      <Layout>
+        <Title>Weather Forecast App</Title>
+        <WeatherCard />
+      </Layout>
       <GlobalStyle />
     </>
   );
--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -1,31 +1,47 @@
 import styled from "styled-components";
 
 const Card = styled.div`
-  background: white;
-  border-radius: 16px;
+  background: rgba(255, 255, 255, 0.8);
+  border-radius: 20px;
   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
-  padding: 2rem;
+  padding: 2rem 3rem;
   text-align: center;
   width: 300px;
+  backdrop-filter: blur(10px);
+  transition:
+    transform 0.2s ease,
+    box-shadow 0.2s ease;
+
+  &:hover {
+    transform: translateY(-5px);
+    box-shadow: 0 12px 25px rgba(0, 0, 0, 0.15);
+  }
 `;
 
 const City = styled.h2`
-  font-size: 1.5rem;
+  font-size: 2rem;
   color: #0077b6;
+  margin-bottom: 0.5rem;
 `;
 
 const Temperature = styled.p`
-  font-size: 2rem;
-  font-weight: bold;
+  font-size: 1.8rem;
+  font-weight: 700;
+  color: #03045e;
+`;
+
+const Description = styled.p`
+  font-size: 1rem;
+  color: #555;
+  margin-top: 0.5rem;
 `;
 
 export default function WeatherCard() {
   return (
-    <>
-      <Card>
-        <City>Irpin</City>
-        <Temperature>+18°C</Temperature>
-      </Card>
-    </>
+    <Card>
+      <City>Irpin</City>
+      <Temperature>+18°C</Temperature>
+      <Description>Sunny</Description>
+    </Card>
   );
 }
