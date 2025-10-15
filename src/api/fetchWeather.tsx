ac63167c28bee7fc8633a83ea2d53f2bb8e634aa
Alozi <alozimozi@gmail.com>
2025-10-15 15:28:11 +0300

    Add fetchForecast to api

--- src/api/fetchWeather.tsx
+++ src/api/fetchWeather.tsx
@@ -1,6 +1,20 @@
-export default async function fetchWeather(API_KEY: string, city: string) {
+export async function fetchWeather(API_KEY: string, city: string) {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+  );
+
+  if (!response.ok) {
+    throw new Error(`Response status: ${response.status}`);
+  }
+
+  const data = await response.json();
+
+  return data;
+}
+
+export async function fetchForecast(API_KEY: string, city: string) {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=imperial`
   );
 
   if (!response.ok) {
