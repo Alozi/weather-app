2491b8d687ec2f990ac77f68337854800b6a80b7
Alozi <alozimozi@gmail.com>
2025-10-21 16:09:31 +0300

    Add getCurrentPosition functionality

--- src/App.tsx
+++ src/App.tsx
@@ -33,12 +33,25 @@ const TitleSecond = styled.h2`
   text-shadow: 1px 1px 4px rgba(0, 0, 0, 0.2);
 `;
 
+const Button = styled.button`
+  padding: 0.5rem 1rem;
+  border-radius: 8px;
+  border: none;
+  background-color: #0077b6;
+  color: #fff;
+  cursor: pointer;
+  transition: background-color 0.2s ease;
+
+  &:hover {
+    background-color: #535bf2;
+  }
+`;
+
 function App() {
   const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
   const [city, setCity] = useState("Lisbon");
   const [weather, setWeather] = useState<WeatherData | null>(null);
   const [forecast, setForecast] = useState<ForecastData[] | null>(null);
-
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
@@ -46,8 +59,8 @@ function App() {
       try {
         setError(null);
 
-        const weatherData = await fetchWeather(API_KEY, city);
-        const forecastData = await fetchForecast(API_KEY, city);
+        const weatherData = await fetchWeather({ API_KEY, city });
+        const forecastData = await fetchForecast({ API_KEY, city });
 
         setWeather(weatherData);
         setForecast(forecastData);
@@ -61,11 +74,44 @@ function App() {
     fetchData();
   }, [API_KEY, city]);
 
+  async function handleCurrentPosition() {
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        console.log("getCurrentPosition");
+
+        const crd = position.coords;
+
+        const lat = crd.latitude;
+        const lon = crd.longitude;
+
+        console.log("Your current position is:");
+        console.log(`Latitude : ${crd.latitude}`);
+        console.log(`Longitude: ${crd.longitude}`);
+        console.log(`More or less ${crd.accuracy} meters.`);
+
+        const weatherData = await fetchWeather({ API_KEY, lat, lon });
+        const forecastData = await fetchForecast({ API_KEY, lat, lon });
+
+        console.log(weatherData);
+
+        setWeather(weatherData);
+        setForecast(forecastData);
+      },
+      (error) => {
+        alert("Unable to retrieve your location 😢");
+        console.error(error);
+      }
+    );
+  }
+
   return (
     <>
       <Layout>
         <Title>Weather Forecast App</Title>
         <SearchBar onSearch={setCity} />
+        <Button type="button" onClick={handleCurrentPosition}>
+          📍 My location
+        </Button>
         {error && <ErrorCard message={error} />}
         {weather && (
           <>
--- src/api/fetchWeather.tsx
+++ src/api/fetchWeather.tsx
@@ -1,22 +1,58 @@
-export async function fetchWeather(API_KEY: string, city: string) {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-  );
+export async function fetchWeather({
+  API_KEY,
+  city,
+  lat,
+  lon,
+}: {
+  API_KEY: string;
+  city?: string;
+  lat?: number;
+  lon?: number;
+}) {
+  let url = "";
 
+  if (lat !== undefined && lon !== undefined) {
+    url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  } else if (city) {
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+  } else {
+    throw new Error("Either city or lat/lon must be provided");
+  }
+
+  const response = await fetch(url);
   if (!response.ok) {
     throw new Error(`Response status: ${response.status}`);
   }
 
   const data = await response.json();
 
+  console.log(data);
+
   return data;
 }
 
-export async function fetchForecast(API_KEY: string, city: string) {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`
-  );
+export async function fetchForecast({
+  API_KEY,
+  city,
+  lat,
+  lon,
+}: {
+  API_KEY: string;
+  city?: string;
+  lat?: number;
+  lon?: number;
+}) {
+  let url = "";
 
+  if (lat !== undefined && lon !== undefined) {
+    url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  } else if (city) {
+    url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`;
+  } else {
+    throw new Error("Either city or lat/lon must be provided");
+  }
+
+  const response = await fetch(url);
   if (!response.ok) {
     throw new Error(`Response status: ${response.status}`);
   }
